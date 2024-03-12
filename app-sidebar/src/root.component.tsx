91c5cf8ad2c1b07f34fdb2da9041d5671c5937a3
Tiago Cardoso <tiago.cardoso@sulamerica.com.br>
2024-03-12 09:25:51 -0300

    chore: add mantine ui

--- app-sidebar/src/root.component.tsx
+++ app-sidebar/src/root.component.tsx
@@ -1,3 +1,26 @@
+import { MantineProvider, createTheme, MantineColorsTuple } from '@mantine/core';
+
+const theme = createTheme({
+  colors: {
+    sula: [
+      "#fff4e2",
+      "#ffe9cc",
+      "#ffd09c",
+      "#fdb766",
+      "#fca13a",
+      "#fb931d",
+      "#fc8c0c",
+      "#e17900",
+      "#c86a00",
+      "#ae5a00"
+    ]
+  }
+})
+
 export default function Root(props) {
-  return <section>{props.name} is mounted! Aqui Sidebar</section>;
+  return (
+    <MantineProvider theme={theme}>
+      <section>{props.name} is mounted! Aqui Sidebar</section>
+    </MantineProvider>
+  );
 }
--- app-sidebar/src/sula-react-sidebar.tsx
+++ app-sidebar/src/sula-react-sidebar.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
 
+import '@mantine/core/styles.css';
+
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
